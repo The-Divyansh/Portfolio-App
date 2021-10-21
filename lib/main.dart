0bd1b9da1bb0bfb3650902f1b55f78426b2fad59
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-10-22 01:15:52 +0530

    Added Project Files

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/dp.jpg'),
+              ),
+              Text(
+                'Divyansh Gupta',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    letterSpacing: 5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[100]),
+              ),
+              Divider(
+                color: Colors.white,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    '+91 89828 56489',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 22,
+                      color: Colors.teal[800],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    'd.gupta.official@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 22,
+                      color: Colors.teal[800],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
